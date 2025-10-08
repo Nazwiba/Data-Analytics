2b6e9a358191ed87c7a3f8fe281169548cf34bcb
Nazwiba <nasweebaa68@gmail.com>
2025-10-08 20:20:12 +0530

    commit

--- MULTIPLICATION TABLE.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Enter the number of which you want the multiplication table: "))
-print("Multiplication Table for", num)
-for i in range(1,11):
-    result = num * i
-    print(num, "*",i, "=" , result)
